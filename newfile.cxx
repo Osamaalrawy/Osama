dd144909ae07a47d5deda2955bf74b7c604925b8
Osamaalrawy <Osamaalrawy511@gmail.com>
2024-08-07 23:44:21 +0300

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void main()
+{
+	/*int x=4;
+	
+	int y=7;
+	 int c=5;
+   int s=x+y+c;
+   int a=s*2;
+   cout<<s;
+	cout<<endl;
+	cout<<a;
+	*/
+/*	int x;
+	int y;
+	cout<<"enter the number of x"<<endl;
+	cin>>x;
+	cout<<"enter the number of y"<<endl;
+	cin>>y;
+	int s=x+y;
+	cout<<"the sum ="<<s<<endl;
+	*/
+/*	float sub1, sub2, sub3, sub4, sum, avg;
+	cout<<"enter the number of sub1"<<endl;
+	cin>>sub1;
+cout<<"enter the number of sub2"<<endl;
+	cin>>sub2;	
+cout<<"enter the number of sub3"<<endl;
+	cin>>sub3;
+	cout<<"enter the number of sub4"<<endl;
+	cin>>sub4;
+sum=sub1+sub2+sub3+sub4;
+cout<<"the mult ="<<sum<<endl;
+avg=sum/4;		
+cout<<"the avrage ="<<avg<<"%"<<endl;	*/
+/*int x=97;
+cout<<(char)x<<endl;		
+*/
+/*char x='c';
+cout<<(int)x<<endl;	*/
+
+/*int x=1, y=2, z=3, temp;
+cout<<"x="<<x<<endl;				
+cout<<"y="<<y<<endl;
+cout<<"z="<<z<<endl;									
+temp=x;			
+x=y;
+y=z;
+z=temp;																								
+cout<<"x="<<x<<endl;				
+cout<<"y="<<y<<endl;
+cout<<"z="<<z<<endl;									*/	
+																										
+ 				
+getch();	
+	
+}
